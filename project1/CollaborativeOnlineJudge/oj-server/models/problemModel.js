cf7fb1968e63c55f1864e36cb4bc7802958eec57
yao <myspiritcrazy@gmail.com>
2017-10-18 22:40:07 -0700

    fixed untracked file (.pyc)

--- project1/CollaborativeOnlineJudge/oj-server/models/problemModel.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-const ProblemSchema = mongoose.Schema({
-    id: Number,
-    name: String,
-    desc: String,
-    difficulty: String
-});
-const ProblemModel = mongoose.model('ProblemModel', ProblemSchema);
-module.exports = ProblemModel;
