590a160bc3b3e1bfb22d059add155ae84678b94d
yao <myspiritcrazy@gmail.com>
2017-10-18 22:28:04 -0700

    pylint and autopep8 to improve code style

--- project1/TapNews/backend_server/service.py
+++ project1/TapNews/backend_server/service.py
@@ -1,24 +1,34 @@
+""" Backend service """
+
 import pyjsonrpc
 
 SERVER_HOST = 'localhost'
 SERVER_PORT = 4040
 
-class RequestHandler(pyjsonrpc.HttpRequestHandler):
 
-    
+class RequestHandler(pyjsonrpc.HttpRequestHandler):
+    """ RPC request handler """
+
     @pyjsonrpc.rpcmethod
-    def add(self, a, b):
+    def add(self, num1, num2): # pylint: disable=no-self-use
         """ Test method """
-        print "Add is called with %d and %d." % (a, b)
-        return a + b
+        print "Add is called with %d and %d." % (num1, num2)
+        return num1 + num2
+
+    @pyjsonrpc.rpcmethod
+    def getOneNews(self): # pylint: disable=no-self-use
+        """ Get one news """
+        print "getOneNews is called"
+        return operations.getOneNews()
+
 
 # Threading HTTP-Server
-http_server = pyjsonrpc.ThreadingHttpServer(
-    server_address = (SERVER_HOST, SERVER_PORT),
-    RequestHandlerClass = RequestHandler
+HTTP_SERVER = pyjsonrpc.ThreadingHttpServer(
+    server_address=(SERVER_HOST, SERVER_PORT),
+    RequestHandlerClass=RequestHandler
 )
 
 
 print "Staring HTTP server on %s:%d" % (SERVER_HOST, SERVER_PORT)
 
-http_server.serve_forever()
+HTTP_SERVER.serve_forever()
