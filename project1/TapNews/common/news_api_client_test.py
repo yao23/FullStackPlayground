0fde696c13ebf027c4d0049dbd5efabc723bee0c
yao <myspiritcrazy@gmail.com>
2017-10-22 23:12:07 -0700

    news api client and news monitor

---
+++ project1/TapNews/common/news_api_client_test.py
@@ -0,0 +1,12 @@
+import news_api_client as client
+
+def test_basic():
+    news = client.getNewsFromSources()
+    print news
+    assert len(news) > 0
+    news = client.getNewsFromSources(sources=['bbc-news'])
+    assert len(news) > 0
+    print 'test_basic passed!'
+
+if __name__ ==  "__main__":
+    test_basic()
