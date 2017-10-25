fe85a5b51c87a2a9dc69e82942800f92e24a421d
yao <myspiritcrazy@gmail.com>
2017-10-24 23:07:36 -0700

    user model with mongoose

--- project1/TapNews/web_server/server/app.js
+++ project1/TapNews/web_server/server/app.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var path = require('path');
+var cors = require('cors');
 
 var index = require('./routes/index');
 var news = require('./routes/news');
-
+var config = require('./config/config.json');
+var _ = require('./models/main.js').connect(config.mongoDbUri); // _ for meaningless variable, no later usage, just for require
 var app = express();
 
 // view engine setup
@@ -13,11 +15,7 @@ app.set('view engine', 'jade');
 app.use('/static', express.static(path.join(__dirname, '../client/build/static/')));
 
 // TODO: remove this after development is done
-app.all('*', function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
-  next();
-});
+app.use(cors());
 
 app.use('/', index);
 app.use('/news', news);
