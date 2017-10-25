dc31ac0209bcefc4d469cb2b5cc140f44cbf0a59
yao <myspiritcrazy@gmail.com>
2017-10-24 21:20:43 -0700

    Base UI

---
+++ project1/TapNews/web_server/client/src/Base/Base.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import Auth from '../Auth/Auth';
+import PropTypes from 'prop-types';
+import './Base.css';
+
+const Base = ({children}) => (
+  <div>
+    <nav className="nav-bar indigo lighten-1">
+      <div className="nav-wrapper">
+        <a href="/" className="brand-logo">  Tap News</a>
+        <ul id="nav-mobile" className="right">
+          {Auth.isUserAuthenticated() ?
+            (<div>
+               <li>{Auth.getEmail()}</li>
+               <li><a href="/logout">Log out</a></li>
+             </div>)
+             :
+            (<div>
+               <li><a href="/login">Log in</a></li>
+               <li><a href="/signup">Sign up</a></li>
+             </div>)
+          }
+        </ul>
+      </div>
+    </nav>
+    <br/>
+    {children}
+  </div>
+);
+
+Base.prototype = {
+    children: PropTypes.object.isRequired
+}
+
+export default Base;
